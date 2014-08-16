c6f08ece6ed5a1a5d019c2a1d074dc8e50b106e2
jianyaoang <jianyaoang1@gmail.com>
2014-08-16 16:58:16 -0500

    Add Open Sesame project

---
+++ Open Sesame/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  Open Sesame
+//
+//  Created by Jian Yao Ang on 8/16/14.
+//  Copyright (c) 2014 Jian Yao Ang. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
---
+++ Open Sesame/ViewController.m
@@ -0,0 +1,29 @@
+//
+//  ViewController.m
+//  Open Sesame
+//
+//  Created by Jian Yao Ang on 8/16/14.
+//  Copyright (c) 2014 Jian Yao Ang. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+	// Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
